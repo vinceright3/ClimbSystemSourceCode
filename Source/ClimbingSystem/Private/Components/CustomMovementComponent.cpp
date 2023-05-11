eddd5d0dabb614071982c042e8d56c76b2972980
Vince Zheng <371096148@qq.com>
2023-05-11 17:06:17 +0800

    EyeHeightTraceDone

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -3,12 +3,14 @@
 
 #include "Components/CustomMovementComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "ClimbingSystem/ClimbingSystemCharacter.h"
 
 void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime,TickType, ThisTickFunction);
 
 	TraceClimbableSurfaces();
+	TraceFromEyeHeight(100.f);
 }
 
 #pragma region ClimbTraces
@@ -34,6 +36,25 @@ TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const F
 	return OutCapsuleTraceHitResults;
 }
 
+FHitResult UCustomMovementComponent::DoLineTraceSingleByObject(const FVector & Start, const FVector & End, bool bShowDebugShape)
+{	
+	FHitResult OutHit;
+
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutHit,
+		false
+	);
+
+	return OutHit;
+}
+
 #pragma endregion
 
 #pragma region ClimbCore
@@ -45,6 +66,17 @@ void UCustomMovementComponent::TraceClimbableSurfaces()
 	const FVector End = Start + UpdatedComponent->GetForwardVector();
 
 	DoCapsuleTraceMultiByObject(Start,End,true);
+}
+
+void UCustomMovementComponent::TraceFromEyeHeight(float TraceDistance, float TraceStartOffset)
+{
+	const FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
+	const FVector EyeHeightOffset = UpdatedComponent->GetUpVector() * (CharacterOwner->BaseEyeHeight + TraceStartOffset);
+	
+	const FVector Start = ComponentLocation + EyeHeightOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector() * TraceDistance;
+
+	DoLineTraceSingleByObject(Start,End,true);
 }
 
 #pragma endregion
--- Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
@@ -22,12 +22,15 @@ private:
 #pragma region ClimbTraces
 	
 	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start,const FVector& End,bool bShowDebugShape = false);
+	FHitResult DoLineTraceSingleByObject(const FVector& Start,const FVector& End,bool bShowDebugShape = false);
 
 #pragma endregion
 
 #pragma region ClimbCore
 	
 	void TraceClimbableSurfaces();
+
+	void TraceFromEyeHeight(float TraceDistance,float TraceStartOffset = 0.f);
 
 #pragma endregion
 

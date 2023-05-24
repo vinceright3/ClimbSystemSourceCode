7f4d8f30d98a40ba2d642e503d3c477da18cadd1
Vince Zheng <371096148@qq.com>
2023-05-24 12:05:46 +0800

    HopDownDone

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -533,21 +533,13 @@ void UCustomMovementComponent::RequestHopping()
 	const float DotResult =
 	FVector::DotProduct(UnrotatedLastInputVector.GetSafeNormal(), FVector::UpVector);
 
-	Debug::Print(TEXT("Dot result: ") + FString::SanitizeFloat(DotResult));
-
 	if(DotResult>=0.9f)
 	{
-		Debug::Print(TEXT("Hop Up"));
-
 		HandleHopUp();
 	}
 	else if(DotResult<=-0.9f)
 	{
-		Debug::Print(TEXT("Hop Down"));
-	}
-	else
-	{
-		Debug::Print(TEXT("Invalid Input Range"));
+		HandleHopDown();
 	}
 }
 
@@ -575,12 +567,38 @@ void UCustomMovementComponent::HandleHopUp()
 
 bool UCustomMovementComponent::CheckCanHopUp(FVector& OutHopUpTargetPosition)
 {	
-	FHitResult HopUpHit = TraceFromEyeHeight(100.f,-30.f,true,true);
-	FHitResult SaftyLedgeHit = TraceFromEyeHeight(100.f,150.f,true,true);
+	FHitResult HopUpHit = TraceFromEyeHeight(100.f,-20.f);
+	FHitResult SaftyLedgeHit = TraceFromEyeHeight(100.f,150.f);
 
 	if(HopUpHit.bBlockingHit && SaftyLedgeHit.bBlockingHit)
 	{	
 		OutHopUpTargetPosition = HopUpHit.ImpactPoint;
+
+		return true;
+	}
+
+	return false;
+}
+
+void UCustomMovementComponent::HandleHopDown()
+{
+	FVector HopDownTargetPoint;
+
+	if(CheckCanHopDown(HopDownTargetPoint))
+	{
+		SetMotionWarpTarget(FName("HopDownTargetPoint"),HopDownTargetPoint);
+
+		PlayClimbMontage(HopDownMontage);
+	}
+}
+
+bool UCustomMovementComponent::CheckCanHopDown(FVector & OutHopDownTargetPosition)
+{
+	FHitResult HopDownHit = TraceFromEyeHeight(100.f,-300.f);
+
+	if(HopDownHit.bBlockingHit)
+	{
+		OutHopDownTargetPosition = HopDownHit.ImpactPoint;
 
 		return true;
 	}
--- Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
@@ -99,6 +99,10 @@ private:
 
 	bool CheckCanHopUp(FVector& OutHopUpTargetPosition);
 
+	void HandleHopDown();
+
+	bool CheckCanHopDown(FVector& OutHopDownTargetPosition);
+
 #pragma endregion
 
 #pragma region ClimbCoreVariables
@@ -157,6 +161,9 @@ private:
 
 	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "Character Movement: Climbing",meta = (AllowPrivateAccess = "true"))
 	UAnimMontage* HopUpMontage;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "Character Movement: Climbing",meta = (AllowPrivateAccess = "true"))
+	UAnimMontage* HopDownMontage;
 
 #pragma endregion
 
